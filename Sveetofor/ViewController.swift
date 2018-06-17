24e89c7dfbb361a5ee17eb2c67da0c320600133a
Екатерина Горбань <soldner166@mail.ru>
2018-06-17 14:29:47 +0300

    Initial Commit

---
+++ Sveetofor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sveetofor
+//
+//  Created by Екатерина Горбань on 17.06.2018.
+//  Copyright © 2018 MIkhail Gorban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
