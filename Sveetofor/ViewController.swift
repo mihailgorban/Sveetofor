45003a51c042d19402a19ca4f46f505d9ce5bef0
Екатерина Горбань <soldner166@mail.ru>
2018-06-20 17:56:36 +0300

    Add buttonTap function for change light

--- Sveetofor/ViewController.swift
+++ Sveetofor/ViewController.swift
@@ -13,11 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var green: UIView!
     @IBOutlet weak var yellow: UIView!
     @IBOutlet weak var red: UIView!
-    
     @IBOutlet weak var button: UIButton!
+    var currentLight = "none"
+    var lightOn: CGFloat = 1
+    var lightOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        red.backgroundColor = UIColor.red
+        red.alpha = lightOff
+        yellow.backgroundColor = UIColor.yellow
+        yellow.alpha = lightOff
+        green.backgroundColor = UIColor.green
+        green.alpha = lightOff
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +34,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonTap(_ sender: Any) {
-    
+    button.setTitle("Next", for: .normal)
+        switch currentLight {
+        case "Green", "none":
+            red.alpha = lightOn
+            yellow.alpha = lightOff
+            green.alpha = lightOff
+            currentLight = "Red"
+        case "Red":
+            red.alpha = lightOff
+            yellow.alpha = lightOn
+            green.alpha = lightOff
+            currentLight = "Yellow"
+        case "Yellow":
+            red.alpha = lightOff
+            yellow.alpha = lightOff
+            green.alpha = lightOn
+            currentLight = "Green"
+        default:
+            currentLight = "none"
+        }
     }
     
 }
